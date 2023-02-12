965a792e725582000ba9edaf020eea07a098527d
Apsil <tumbler.m.r1@gmail.com>
2023-02-12 10:22:06 +1000

    buttons (temporary solution)
    Временное решение запихать кнопки в старт (потом изменить на другую функцию)

--- main.py
+++ main.py
@@ -9,27 +9,27 @@ def start(message):
     sMess = f'<b>Привет, {message.from_user.first_name} {message.from_user.last_name} я - FundaBot</b>'
     bot.send_message(message.chat.id, sMess, parse_mode='html')
     bot.send_message(message.chat.id, '<b> Вот список моих комманд </b>', parse_mode='html')
+    bot.send_message(message.chat.id,
+                     '<b>1. Лабораторная работа 2.1\n'
+                     '2. Лабораторная работа 2.2\n'
+                     '3. Лабораторная работа 2.3\n'
+                     '4. Лабораторная работа 2.4\n'
+                     '5. Курсовая работа по ФСДИА</b>',
+                     parse_mode='html')
+
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
+    btn1 = types.KeyboardButton("2.1")
+    btn2 = types.KeyboardButton("2.2")
+    btn3 = types.KeyboardButton("2.3")
+    btn4 = types.KeyboardButton("2.4")
+    btn5 = types.KeyboardButton("Курсовая работа")
+    markup.add(btn1, btn2, btn3, btn4, btn5)
+    bot.send_message(message.chat.id, "Что ты хочешь посмотреть?", reply_markup=markup)
 
 
 @bot.message_handler(commands=['help'])
 def help(message):
-    bot.send_message(message.chat.id,
-                     '<b>Список моих функций:</b>\n'
-                     '<i>1. Лабораторная работа 2.1\n'
-                     '2. Лабораторная работа 2.2\n'
-                     '3. Лабораторная работа 2.3\n'
-                     '4. Лабораторная работа 2.4\n'
-                     '5. Курсовая работа по ФСДИА</i>',
-                     parse_mode='html')
-
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
-    btn1 = types.KeyboardButton("Лабораторная работа 2.1")
-    btn2 = types.KeyboardButton("Лабораторная работа 2.2")
-    btn3 = types.KeyboardButton("Лаборатотная работа 2.3")
-    btn4 = types.KeyboardButton("Лабораторная работа 2.4")
-    btn5 = types.KeyboardButton("Курсовая работа по ФСДИА")
-    markup.add(btn1, btn2, btn3, btn4, btn5)
-    bot.send_message(message.chat.id, "Что ты хочешь посмотреть?", reply_markup=markup)
+    bot.send_message(message.chat.id, "Временно недоступно")
 
 
 bot.polling(none_stop=True)
